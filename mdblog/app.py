2c7cac85a86aed520a5ee70a4a627ea70fa00550
RadoS <radovanse@gmail.com>
2021-02-17 15:21:27 +0000

    initial commit

--- mdblog/app.py
+++ mdblog/app.py
@@ -4,9 +4,17 @@ from flask import render_template
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
-def view():
+def welcome_page():
     return render_template("welcome_page.jinja")
 
 @flask_app.route("/about/")
-def view_about():
+def about_page():
     return render_template("about_page.jinja")
+
+@flask_app.route("/admin/")
+def admin_page():
+    return render_template("admin_page.jinja")
+
+@flask_app.route("/articles/")
+def article_page():
+    return render_template("articles_page.jinja")
